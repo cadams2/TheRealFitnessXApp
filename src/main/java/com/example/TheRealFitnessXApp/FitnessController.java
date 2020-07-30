d22f8c74e30e27bdf30c7b4508b728285f92928d
Cameron Adams <cameronpa1@yahoo.com>
2020-07-30 16:07:51 -0400

    changed line 12 to the correct word: was "fitnexxappx" to "fitnessappx"

--- src/main/java/com/example/TheRealFitnessXApp/FitnessController.java
+++ src/main/java/com/example/TheRealFitnessXApp/FitnessController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/fitnexxappx")
+@RequestMapping("/fitnessappx")
 public class FitnessController {
 
     @Autowired
