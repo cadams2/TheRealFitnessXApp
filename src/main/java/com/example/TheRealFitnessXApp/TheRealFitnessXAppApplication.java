ddf76b752762769c77c17d7746c574d277e008af
Cameron Adams <cameronpa1@yahoo.com>
2020-07-30 14:27:41 -0400

    Start of the Project

---
+++ src/main/java/com/example/TheRealFitnessXApp/TheRealFitnessXAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.TheRealFitnessXApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRealFitnessXAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRealFitnessXAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TheRealFitnessXApp/TheRealFitnessXAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TheRealFitnessXApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRealFitnessXAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
