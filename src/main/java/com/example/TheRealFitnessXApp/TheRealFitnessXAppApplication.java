c16620d5d07576869de75d77b23739a04bbc7f23
Cameron Adams <cameronpa1@yahoo.com>
2020-07-31 10:34:01 -0400

    added system file

--- src/main/java/com/example/TheRealFitnessXApp/TheRealFitnessXAppApplication.java
+++ src/main/java/com/example/TheRealFitnessXApp/TheRealFitnessXAppApplication.java
@@ -2,7 +2,9 @@ package com.example.TheRealFitnessXApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 public class TheRealFitnessXAppApplication {
 
